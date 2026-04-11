7e24dd1f968aa452e8b574be79f30ef4d3f3d436
Eric Hendershot <ericolebusiness@gmail.com>
2026-04-11 18:59:00 -0400

    dump .dat into json format

--- extract.py
+++ extract.py
@@ -1,30 +1,49 @@
-import nbtlib
 from datetime import datetime
-from pprint import pprint
+from config import root_path
+import nbtlib
+import json
+import os
+
+timestamp = datetime.now().strftime('%Y-%m-%d')
+dump_path = f'dump/{timestamp}'
+os.makedirs(dump_path, exist_ok=True)
+
+# .dat default for converting objects to json readable format
+def nbt_serialize(obj):
+    if isinstance(obj, (list, tuple)):
+        return list(obj)
+    if isinstance(obj, dict):
+        return {k: nbt_serialize(v) for k, v in obj.items()}
+    if isinstance(obj, (int, float, str, bool, type(None))):
+        return obj
+    if hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes)):
+        return list(obj)
+    return str(obj)
+
+# dump .dat into a json file
+def dump_dat(dat, file_name):
+  try:
+    json.dump(dat, open(f'{dump_path}/{file_name}', 'w'), indent=2, default=nbt_serialize)
+    print(f'{file_name}.json saved')
+  except (IOError, OSError) as e:
+    print(f'File error: {e}')
+  except TypeError as e:
+    print(f'Cant serialize: {e}')
+  except Exception as e:
+    print(f'Unknown error: {e}')
 
 # path to .dat files
-path_level_dat = '/Users/ehen/Library/Application Support/PrismLauncher/instances/26.1.2/minecraft/saves/etl/level.dat'
+world_name = 'etl'
+level_dat_path = f'{root_path}/minecraft/saves/{world_name}/level.dat'
+player_dat_dir = f'{root_path}/minecraft/saves/{world_name}/players/data/'
 
-# load .dat
-level_dat = nbtlib.load(path_level_dat)
+# get world .dat file
+level_dat = nbtlib.load(level_dat_path)
+dump_dat(level_dat, 'level_dat')
 
-# pull out desired data
-world_name = level_dat['Data']['LevelName']
-world_type = level_dat['Data']['GameType']
-world_difficulty = level_dat['Data']['difficulty_settings']['difficulty']
-is_hardcore = level_dat['Data']['difficulty_settings']['hardcore']
-current_dimension = level_dat['Data']['spawn']['dimension']
-last_played = level_dat['Data']['LastPlayed']
-
-# dictionary of data
-raw_data = {
-  'world name': world_name,
-  'world type': world_type,
-  'hardcore': is_hardcore,
-  'world difficulty': world_difficulty,
-  'current dimension': current_dimension,
-  'last played': last_played,
-}
-
-# print dict
-pprint(raw_data, indent=4, width=50, sort_dicts=False)
+# get each players .dat file
+for player_file in os.listdir(player_dat_dir):
+  if player_file.endswith('.dat'):
+    player_dat = nbtlib.load(os.path.join(player_dat_dir, player_file))
+    uuid = player_file.removesuffix('.dat')
+    dump_dat(player_dat, f'player_{uuid}.json')
